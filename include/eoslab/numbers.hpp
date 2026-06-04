1501a7677a66edd1d41f8de390ff33b274392869
Danny Long <dannylongcsu@gmail.com>
2026-06-04 09:12:44 +0200

    add docs

--- include/eoslab/numbers.hpp
+++ include/eoslab/numbers.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
+/**
+ * @file numbers.hpp
+ * @brief Fundamental physical constants used throughout the library.
+ */
+
 #include <concepts>
 namespace glis::eos {
 
+/**
+ * @brief The universal (molar) gas constant @f$R@f$.
+ *
+ * @tparam Number Floating-point type the constant is expressed in.
+ *
+ * Value: `8.314462618`.
+ * Units: **J / (mol K)** (joule per mole per kelvin).
+ */
 template<std::floating_point Number = double> inline constexpr Number ideal_gas_constant{8.314462618};
 
-}
+} // namespace glis::eos
